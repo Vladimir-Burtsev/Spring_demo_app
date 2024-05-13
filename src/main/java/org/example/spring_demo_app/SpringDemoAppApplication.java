2b4586a3c01f29092107c868c53f430d08cd23c8
vladimir burtsev <amakusl2@gmail.com>
2024-05-13 17:10:13 +0300

    Инициализирован каркас проекта, добавлены конфиги для доступа к БД и настройки ORM.

--- src/main/java/org/example/spring_demo_app/SpringDemoAppApplication.java
+++ src/main/java/org/example/spring_demo_app/SpringDemoAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDemoAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoAppApplication.class, args);
+    }
 
 }
