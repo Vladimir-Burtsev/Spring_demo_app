f9f7494795ea4950161fe295d9480de2d39e18f5
vladimir burtsev <amakusl2@gmail.com>
2024-05-13 17:07:46 +0300

    Initial commit

---
+++ src/test/java/org/example/spring_demo_app/SpringDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring_demo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
