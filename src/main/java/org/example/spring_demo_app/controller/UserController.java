d7676200131b3a53caebc83cd90699975c0291f3
vladimir burtsev <amakusl2@gmail.com>
2024-05-15 00:16:36 +0300

    Инжект сервиса через интерфейс.

--- src/main/java/org/example/spring_demo_app/controller/UserController.java
+++ src/main/java/org/example/spring_demo_app/controller/UserController.java
@@ -1,8 +1,8 @@
-package org.example.spring_demo_app.Controller;
+package org.example.spring_demo_app.controller;
 
 import jakarta.validation.Valid;
-import org.example.spring_demo_app.Model.User;
-import org.example.spring_demo_app.Service.UserService;
+import org.example.spring_demo_app.model.User;
+import org.example.spring_demo_app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,39 +24,45 @@ public class UserController {
         model.addAttribute("users", userService.findAllUsers());
         return "AllUsers";
     }
+
     @GetMapping("{id}")
     public String findById(@PathVariable int id, Model model) {
         model.addAttribute("user", userService.findUserById(id));
         return "UserDetails";
     }
+
     @GetMapping("/new")
     public String newUser(Model model) {
         model.addAttribute("user", new User());
         return "NewUser";
     }
+
     @PostMapping()
     public String createUser(@ModelAttribute("user") @Valid User user,
-                             BindingResult bindingResult){
-        if (bindingResult.hasErrors()){
+                             BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
             return "NewUser";
         }
         userService.saveUser(user);
         return "redirect:/users";
     }
+
     @GetMapping("/edit")
-    public String edit(Model model, @RequestParam(value = "id") long id){
+    public String edit(Model model, @RequestParam(value = "id") long id) {
         model.addAttribute("user", userService.findUserById(id));
         return "Edit";
     }
+
     @PostMapping("/edit/{id}")
     public String update(@ModelAttribute("user") @Valid User user,
-                         BindingResult bindingResult, @RequestParam("id") long id){
-        if (bindingResult.hasErrors())
+                         BindingResult bindingResult, @RequestParam("id") long id) {
+        if (bindingResult.hasErrors()) {
             return "Edit";
-
+        }
         userService.updateUser(user);
         return "redirect:/users";
     }
+
     @PostMapping("/delete")
     public String deleteUser(@RequestParam("id") long id) {
         userService.deleteUserById(id);
