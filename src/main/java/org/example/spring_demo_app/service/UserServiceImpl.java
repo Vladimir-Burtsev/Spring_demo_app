de37240712a8d09889cbd60c481460e997fb2c2e
vladimir burtsev <amakusl2@gmail.com>
2024-05-15 00:19:33 +0300

    Удалена проверка на null.

--- src/main/java/org/example/spring_demo_app/service/UserServiceImpl.java
+++ src/main/java/org/example/spring_demo_app/service/UserServiceImpl.java
@@ -1,30 +1,42 @@
-package org.example.spring_demo_app.Service;
+package org.example.spring_demo_app.service;
 
-import org.example.spring_demo_app.Model.User;
-import org.example.spring_demo_app.Repository.UserRepository;
+import org.example.spring_demo_app.model.User;
+import org.example.spring_demo_app.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
-public class UserService {
+public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    @Override
     public User findUserById(long id) {
-        return userRepository.findById(id).orElse(null);
+        return userRepository.findById(id).get();
     }
+
+    @Override
     public List<User> findAllUsers() {
         return userRepository.findAll();
     }
+
+    @Override
     public User saveUser(User user) {
         return userRepository.save(user);
     }
+
+    @Override
     public void deleteUserById(long id) {
         userRepository.deleteById(id);
     }
+
+    @Override
     public User updateUser(User user) {
         return userRepository.save(user);
     }
