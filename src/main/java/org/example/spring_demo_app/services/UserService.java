aa28ffa88bb76fcdb2f70a052cc8a0bacaf40447
vladimir burtsev <amakusl2@gmail.com>
2024-05-25 18:38:07 +0300

    Добавил метод findByUsername.

--- src/main/java/org/example/spring_demo_app/services/UserService.java
+++ src/main/java/org/example/spring_demo_app/services/UserService.java
@@ -1,11 +1,13 @@
-package org.example.spring_demo_app.service;
+package org.example.spring_demo_app.services;
 
-import org.example.spring_demo_app.model.User;
+import org.example.spring_demo_app.models.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
     User findUserById(long id);
+    Optional<User> findByUsername(String username);
 
     List<User> findAllUsers();
 
@@ -14,4 +16,5 @@ public interface UserService {
     void deleteUserById(long id);
 
     User updateUser(User user);
+
 }
