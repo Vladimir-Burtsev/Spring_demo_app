dbdfdcc790e8b4c35cbdeb3d79e939960f6dd11b
vladimir burtsev <amakusl2@gmail.com>
2024-05-26 18:58:50 +0300

    Инициализация пустой БД дефолтным админом.

--- src/main/java/org/example/spring_demo_app/util/AutoCreateSuperAdmin.java
+++ src/main/java/org/example/spring_demo_app/util/AutoCreateSuperAdmin.java
@@ -1,50 +1,50 @@
-//package org.example.spring_demo_app.util;
-//
-//import org.example.spring_demo_app.models.Role;
-//import org.example.spring_demo_app.models.User;
-//import org.example.spring_demo_app.repositories.RoleRepository;
-//import org.example.spring_demo_app.repositories.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.stereotype.Component;
-//
-//import java.util.Set;
-//
-//@Component
-//public class AutoCreateSuperAdmin implements CommandLineRunner {
-//    private final UserRepository userRepository;
-//
-//    private final RoleRepository roleRepository;
-//
-//    private final BCryptPasswordEncoder encoder;
-//    @Autowired
-//    public AutoCreateSuperAdmin(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder encoder) {
-//        this.userRepository = userRepository;
-//        this.roleRepository = roleRepository;
-//        this.encoder = encoder;
-//    }
-//    @Override
-//    public void run(String... args) throws Exception {
-//        if (!userRepository.existsByUsername("admin")) {
-//            Role adminRole = roleRepository.findByRole("ADMIN");
-//            if (adminRole == null) {
-//                adminRole = new Role();
-//                adminRole.setRole("ADMIN");
-//                roleRepository.save(adminRole);
-//            }
-//            Role userRole = roleRepository.findByRole("USER");
-//            if (userRole == null) {
-//                userRole = new Role();
-//                userRole.setRole("USER");
-//                roleRepository.save(userRole);
-//            }
-//
-//            User adminUser = new User();
-//            adminUser.setUsername("admin");
-//            adminUser.setPassword(encoder.encode("admin"));
-//            adminUser.setRoles(Set.of(adminRole, userRole));
-//            userRepository.save(adminUser);
-//        }
-//    }
-//}
+package org.example.spring_demo_app.util;
+
+import org.example.spring_demo_app.models.Role;
+import org.example.spring_demo_app.models.User;
+import org.example.spring_demo_app.repositories.RoleRepository;
+import org.example.spring_demo_app.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class AutoCreateSuperAdmin implements CommandLineRunner {
+    private final UserRepository userRepository;
+
+    private final RoleRepository roleRepository;
+
+    private final BCryptPasswordEncoder encoder;
+    @Autowired
+    public AutoCreateSuperAdmin(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder encoder) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+        this.encoder = encoder;
+    }
+    @Override
+    public void run(String... args) throws Exception {
+        if (!userRepository.existsByUsername("admin")) {
+            Role adminRole = roleRepository.findByRole("ADMIN");
+            if (adminRole == null) {
+                adminRole = new Role();
+                adminRole.setRole("ADMIN");
+                roleRepository.save(adminRole);
+            }
+            Role userRole = roleRepository.findByRole("USER");
+            if (userRole == null) {
+                userRole = new Role();
+                userRole.setRole("USER");
+                roleRepository.save(userRole);
+            }
+
+            User adminUser = new User();
+            adminUser.setUsername("admin");
+            adminUser.setPassword(encoder.encode("admin"));
+            adminUser.setRoles(Set.of(adminRole, userRole));
+            userRepository.save(adminUser);
+        }
+    }
+}
