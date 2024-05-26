f931da5387663871b3252021f6fa765f35b0eca1
vladimir burtsev <amakusl2@gmail.com>
2024-05-26 10:24:36 +0300

    Настроены конфиги для аутинтификации пользователя и ограничения доступов по ролям. Также реализован энкодер.

---
+++ src/main/java/org/example/spring_demo_app/configs/WebSecurityConfig.java
@@ -0,0 +1,50 @@
+package org.example.spring_demo_app.configs;
+
+import org.example.spring_demo_app.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    private final SuccessUserHandler successUserHandler;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final BCryptPasswordEncoder encoder;
+    @Autowired
+    public WebSecurityConfig(SuccessUserHandler successUserHandler,
+                             UserDetailsServiceImpl userDetailsService,
+                             BCryptPasswordEncoder encoder) {
+        this.successUserHandler = successUserHandler;
+        this.userDetailsService = userDetailsService;
+        this.encoder = encoder;
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())  // Отключение CSRF защиты
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .successHandler(successUserHandler)
+                        .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+    @Autowired
+    public void AuthUserDetailsService(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+    }
+}
