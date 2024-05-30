d296c5558d17d9946371cdd44e79aaed9ca1b0b3
vladimir burtsev <amakusl2@gmail.com>
2024-05-30 21:15:01 +0300

    рефакторинг

--- src/main/java/org/example/spring_demo_app/controllers/UserController.java
+++ src/main/java/org/example/spring_demo_app/controllers/UserController.java
@@ -1,37 +1,26 @@
 package org.example.spring_demo_app.controllers;
 
-import jakarta.validation.Valid;
 import org.example.spring_demo_app.models.User;
-import org.example.spring_demo_app.services.RoleService;
 import org.example.spring_demo_app.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.NoSuchElementException;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
-    private final RoleService roleService;
 
     @Autowired
-    public UserController(UserService userService, RoleService roleService) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.roleService = roleService;
     }
 
     @GetMapping()
-    public String findById(@RequestParam("id") int id, Model model) {
-        try {
-            User user = userService.findUserById(id);
-            model.addAttribute("user", user);
-            return "UserDetails";
-        } catch (NoSuchElementException e) {
-            return "NotFound";
-        }
+    public String findById(@RequestParam("id") long id, Model model) {
+        User user = userService.findUserById(id);
+        model.addAttribute("user", user);
+        return "user/user-bootstrap";
     }
 }
